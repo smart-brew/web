06a7255456833f2da794df340b8e5fc80b2dd4ca
Peter Striz <petulak8@gmail.com>
2021-10-29 18:02:38 +0200

    feature(SB-22): updated webpage with font and text

--- main.js
+++ main.js
@@ -1,32 +1,32 @@
-const menu = document.querySelector(".menu");
+const menu = document.querySelector('.menu');
 
-const domov = document.getElementById("domov");
-const projekt = document.getElementById("projekt");
-const clenovia = document.getElementById("clenovia");
-const milniky = document.getElementById("milniky");
-const casova_os = document.getElementById("casova-os");
-const dokumenty = document.getElementById("dokumenty");
+const domov = document.getElementById('domov');
+const projekt = document.getElementById('projekt');
+const clenovia = document.getElementById('clenovia');
+const milniky = document.getElementById('milniky');
+const casova_os = document.getElementById('casova-os');
+const dokumenty = document.getElementById('dokumenty');
 
 const pole = [domov, projekt, clenovia, milniky, casova_os, dokumenty];
 
 var currentActiveMenu = menu.querySelector(`.domov`);
 
-window.addEventListener("scroll", (ev) => {
+window.addEventListener('scroll', (ev) => {
   var najlepsie = domov;
   pole.forEach((el) => {
     const elY = el.getBoundingClientRect().y;
     const najlepsieY = domov.getBoundingClientRect().y;
-    if (elY < 10 && elY > najlepsieY) {
+    if (elY < 180 && elY > najlepsieY) {
       najlepsie = el;
     }
   });
 
   const activeMenu = menu.querySelector(`.${najlepsie.id}`);
 
-  currentActiveMenu.classList.add("border-transparent");
-  currentActiveMenu.classList.remove("border-black");
-  activeMenu.classList.remove("border-transparent");
-  activeMenu.classList.add("border-black");
+  currentActiveMenu.classList.add('border-transparent');
+  currentActiveMenu.classList.remove('border-black');
+  activeMenu.classList.remove('border-transparent');
+  activeMenu.classList.add('border-black');
 
   currentActiveMenu = activeMenu;
 });
